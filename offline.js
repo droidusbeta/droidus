dbe0416b6d6536cd58b1ec2e3fd64b1c11861e17
droidusbeta <37776610+droidusbeta@users.noreply.github.com>
2018-03-25 17:38:40 +0200

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1521934990,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/fons2-sheet0.png",
+		"images/jugarboto-sheet0.png",
+		"images/ajudaboto-sheet0.png",
+		"images/conboto-sheet0.png",
+		"images/fons-sheet0.png",
+		"images/batalla1-sheet0.png",
+		"images/batalla2-sheet0.png",
+		"images/enrere-sheet0.png",
+		"images/fons4-sheet0.png",
+		"images/enrere3-sheet0.png",
+		"images/r1-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js"
+	]
+}
